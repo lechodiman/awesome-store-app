369d64d29c1e9516561871fb06d45c6b3fa8e635
Luis Chodiman <lechodiman@uc.cl>
2020-12-09 12:00:57 -0300

    feat: add product form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Box, Center, Flex, Heading, Image } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+  Text,
+  Textarea,
+  useDisclosure,
+  VisuallyHidden,
+} from '@chakra-ui/react';
 import './App.css';
 
 function App() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <div>
       <Box as="nav" bgColor="gray.800">
@@ -22,14 +51,19 @@ function App() {
 
       <Box as="header" bgColor="white" boxShadow="md">
         <Box maxWidth="6xl" mx="auto" py="6" px="4">
-          <Heading
-            fontWeight="bold"
-            letterSpacing="tight"
-            color="gray.900"
-            size="lg"
-          >
-            Productos
-          </Heading>
+          <Flex justify="space-between">
+            <Heading
+              fontWeight="bold"
+              letterSpacing="tight"
+              color="gray.900"
+              size="lg"
+            >
+              Productos
+            </Heading>
+            <Button colorScheme="purple" onClick={onOpen}>
+              Crear
+            </Button>
+          </Flex>
         </Box>
       </Box>
 
@@ -46,6 +80,107 @@ function App() {
           </Box>
         </Box>
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay></ModalOverlay>
+        <ModalContent>
+          <ModalHeader>Nuevo Producto</ModalHeader>
+          <ModalCloseButton></ModalCloseButton>
+          <ModalBody>
+            <form>
+              <FormControl id="name">
+                <FormLabel>Nombre</FormLabel>
+                <Input></Input>
+              </FormControl>
+
+              <FormControl id="price">
+                <FormLabel>Precio</FormLabel>
+                <NumberInput>
+                  <NumberInputField></NumberInputField>
+                  <NumberInputStepper>
+                    <NumberIncrementStepper></NumberIncrementStepper>
+                    <NumberDecrementStepper></NumberDecrementStepper>
+                  </NumberInputStepper>
+                </NumberInput>
+              </FormControl>
+
+              <FormControl id="description">
+                <FormLabel>Descripción</FormLabel>
+                <Textarea placeholder="Una breve descripción del producto."></Textarea>
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Imagen</FormLabel>
+                <Box
+                  d="flex"
+                  justifyContent="center"
+                  px="6"
+                  pt="5"
+                  pb="6"
+                  border="2px"
+                  borderColor="gray.300"
+                  borderStyle="dashed"
+                  borderRadius="md"
+                >
+                  <Stack spacing="1" textAlign="center">
+                    <Box
+                      as="svg"
+                      mx="auto"
+                      h="12"
+                      w="12"
+                      color="gray.400"
+                      h-12
+                      w-12
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </Box>
+                    <Box
+                      d="flex"
+                      fontSize="sm"
+                      color="gray.600"
+                      alignItems="baseline"
+                    >
+                      <FormLabel
+                        cursor="pointer"
+                        color="purple.500"
+                        htmlFor="file-upload"
+                        mr="1"
+                      >
+                        Sube una imágen
+                      </FormLabel>
+                      <Text fontSize="xs">o arrastra y suelta acá.</Text>
+                      <VisuallyHidden>
+                        <input
+                          type="file"
+                          id="file-upload"
+                          name="file-upload"
+                        />
+                      </VisuallyHidden>
+                    </Box>
+                  </Stack>
+                </Box>
+              </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button variant="ghost" onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button ml="3" colorScheme="purple" onClick={onClose}>
+              Crear
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
